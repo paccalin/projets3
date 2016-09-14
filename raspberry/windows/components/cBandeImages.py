ce8ce9e4670f4a75ee23b41f9a31d45581eeaa9c
Alexandre <alexandre.moreau2@etu.univ-smb.fr>
2016-09-14 15:29:37 +0200

    ajout de la fenêtre bandeimage

---
+++ raspberry/windows/components/cBandeImages.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from windows.classes.appWindow import *
+
+#fenêtre diaporama
+class CImgDiapo(QWidget):
+    def __init__(self):
+        QWidget.__init__(self):
