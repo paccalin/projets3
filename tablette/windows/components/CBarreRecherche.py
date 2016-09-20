6aeab38923c6386f1f109b9d9c16f2dbdef39381
Axel Paccalin <paccalin.axel@gmail.com>
2016-09-20 16:17:56 +0200

    ajout du composant recherche

---
+++ tablette/windows/components/CBarreRecherche.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from windows.classes.appWindow import *
+
+#fenêtre diaporama
+class CBandeImage(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.__windowStruct = appWindow()
+
+    def ScaleContent(self):
+        echo("Edouard doit faire son taff")
