9a39eb431ad5cceebeba0f77edb33c5a8e47ed3a
Axel Paccalin <paccalin.axel@gmail.com>
2016-10-19 14:26:43 +0200

    correction de bugs de youssouf :(

--- raspberry/windows/components/CDescription.py
+++ raspberry/windows/components/CDescription.py
@@ -29,26 +29,20 @@ class CDescription(QWidget):
     def ScaleContent(self):
         windowSize = self.__windowStruct.Size()
         self.labeltitre.move(0.25*windowSize.X(),0.05*windowSize.Y())
-        self.labeltitre.show()
         self.labelMarque.move(0.25*windowSize.X(),0.15*windowSize.Y())
-        self.labelMarque.show()
         self.labelModele.move(0.60*windowSize.X(),0.15*windowSize.Y())
-        self.labelModele.show()
         self.labelOption.move(0.40*windowSize.X(), 0.30*windowSize.Y())
-        self.labelOption.show()
 
     def Update(self, pImg):
         windowSize = self.__windowStruct.Size()
         currentVehicle = pImg.Vehicle()
         if (currentVehicle==None):
             self.labelVide.move(0.05*windowSize.X(),0.40*windowSize.Y())
-            self.labelVide.show()
         else:
 
             self.labelVide.hide()
             self.labelLibelle = QLabel(" " + pImg.Vehicle().Model().Libelle())
             self.labelLibelle.move(0.25*windowSize.X(),0.50*windowSize.Y())
-            self.labelLibelle.show()
             self.labelMarque.setText(" " + currentVehicle.Model().Manufacturer().Libelle())
             self.labelModele.setText(" " + currentVehicle.Model().Libelle())
 
