a224d6e07acecd2c9426ffa33e4e58a2191c23df
Etilis <etilisdu74@yahoo.fr>
2016-12-01 11:41:28 +0100

    correction de l'emplacement de insert.php et renomé allInsert.php

---
+++ tablette_web/view/insert/allInsert.php
@@ -0,0 +1,41 @@
+<?php
+	
+echo <<<HTML
+	<form action ='{a definir}' method='post'>
+		<select>
+			<option value='null'>--- choisir ---</option>
+			<option value='client'>client</option>
+			<option value='rdv'>rendez-vous</option>php
+			<option value='devis'>devis</option>			
+HTML;
+
+/*
+if({utilisateur_droit} == '{administrateur}'){
+	echo <<<HTML
+		<option value='vehicule'>vehicule</option>
+		<option value='photo'>photo</option>
+		<option value='option'>option</option>
+		<option value='brand'>constructeur</option>
+		<option value='modele'>modele</option>
+HTML;
+}
+
+
+if({utilisateur_droit} == '{super_admin}'){
+	echo "<option value='user'>utilisateur</option>";
+}
+*/
+
+echo <<<HTML
+
+		</select>
+
+		<input type='submit' value='valider' name='submit' id='submit'/>
+
+	</form>
+
+HTML;
+?>
+
+
+<script type='text/javascript' src='./js/insert.js'></script>
