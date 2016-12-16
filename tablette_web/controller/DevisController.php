30cd7dca18313a406949bedd487001a33b707978
Etilis <etilisdu74@yahoo.fr>
2016-12-16 11:29:53 +0100

    devis fonctionnel pour marque->modele

--- tablette_web/controller/DevisController.php
+++ tablette_web/controller/DevisController.php
@@ -1,6 +1,14 @@
 <?php
 class DevisController extends Controller{
 	public function show(){
-		$this->render("show");
+		$constructeurs = Constructeur::FindAll();
+		
+		$modeles = Modele::FindAll();
+		
+		//$modele->constructeur;
+		
+		$data = ['constructeurs'=>$constructeurs, 'modeles'=>$modeles];
+		
+		$this->render("show", $data);
 	}
 }
