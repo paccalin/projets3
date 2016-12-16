195954a312341d2088c24860f3f0d52d77e936df
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2016-12-16 12:31:21 +0100

    Gestion du menu défilant dans le PHP

--- tablette_web/view/header.php
+++ tablette_web/view/header.php
@@ -5,13 +5,49 @@
 		<!-- header ici  -->
 		<nav>
 			<ul>
-				<a href="./?r=site/index""><img src='./images/logo.jpg' alt='ID AMENAGEMENT UTILITAIRES' id='home'/></a>
+				<a href='./?r=site/index'><img src='./images/logo.jpg' alt='ID AMENAGEMENT UTILITAIRES' id='home'/></a>
 				<img src='./images/menu.png' alt='menu' id='menuButton'/ class="imageButton">
 			</ul>
 		</nav>
-		<?php
-			
-		?>
+		<div id='menuDiv' class='cache'>
+			<ul id='menuUl'>
+				<?php
+					$home = "./?r=site/index";
+					$devis = "./?r=devis/show";
+					$maj = "./?r=site/aFaire";
+					$rdv = "./?r=site/aFaire";
+					$add = "./?r=insert/viewInsert";
+					$del = "./?r=site/aFaire";
+					$upd = "./?r=site/aFaire";
+					$diapo = "./?r=Diapo/view_diapo";			
+					
+					echo "<a href='".$home."' class='lien'>\n\t\t\t\t\t<li class='menu'>Accueil</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+					echo "\n\t\t\t\t<a href='".$diapo."' class='lien'>\n\t\t\t\t\t<li class='menu'>Diapo</li>\n\t\t\t\t</a><hr class='separateurPetit'>";
+					if($_SESSION['droits']>=1){
+						echo "\n\t\t\t\t<a href='".$maj."' class='lien'>\n\t\t\t\t\t<li class='menu'>Mise à jour</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'/>";
+						echo "\n\t\t\t\t<a href='".$rdv."'' class='lien'>\n\t\t\t\t\t<li class='menu'>Rendez-vous</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'/>";
+						echo "\n\t\t\t\t<a href='".$add."' class='lien'>\n\t\t\t\t\t<li class='menu'>Ajouter</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+						echo "\n\t\t\t\t<a href='".$del."' class='lien'>\n\t\t\t\t\t<li class='menu'>Supprimer</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+						echo "\n\t\t\t\t<a href='".$upd."' class='lien'>\n\t\t\t\t\t<li class='menu'>Modifier</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurGrand'>";
+					}
+					if($_SESSION['droits']==0){
+						echo "\n\t\t\t\t<a href='".'./?r=connexion/formConnexion'."' class='lien'>\n\t\t\t\t\t<li class='menu'>Se connecter</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+					}else{
+						if($_SESSION['droits']>= 2){
+							echo "\n\t\t\t\t<a href='".'./?r=administration/creerCompte'."' class='lien'>\n\t\t\t\t\t<li class='menu'>Créer un Compte</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+							if($_SESSION['droits']==2){
+								echo "\n\t\t\t\t<a href='".'./?r=administration/gererComptes'."' class='lien'>\n\t\t\t\t\t<li class='menu'>Afficher les Comptes</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+							}else{
+								echo "\n\t\t\t\t<a href='".'./?r=administration/gererComptes'."' class='lien'>\n\t\t\t\t\t<li class='menu'>Gérer les Comptes</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+							}
+						}
+						echo "\n\t\t\t\t<a href='".'./?r=administration/changerMotPasse'."' class='lien'>\n\t\t\t\t\t<li class='menu'>Changer le mot de passe</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+						echo "\n\t\t\t\t<a href='".'./?r=connexion/deconnexion'."' class='lien'>\n\t\t\t\t\t<li class='menu'>Se déconnecter</li>\n\t\t\t\t</a>\n\t\t\t\t<hr class='separateurPetit'>";
+						echo "\n";
+					}
+				?>
+			</ul>
+		</div>
 	</header>
 
 
