fa25148d2d9e933644698c12c1fd233b402cf1c6
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2017-01-22 15:46:05 +0100

    Ajout de trucs de management d'équipe

--- tablette_web/view/site/index.php
+++ tablette_web/view/site/index.php
@@ -3,6 +3,8 @@
 <p><i>(R) page à réaliser</i></p>
 <p><i>(F) page à finaliser</i></p>
 <br/>
-<p><u><b>A faire Alexis/Joseph/Edouard:</b></u><br/>-Ajouter le regex sur la création de client (traiter les erreurs au bon endroit)<br/>-Controler tous les formulaires de création puis modification et noter leurs erreurs dans un fichier txt (mauvais traitemenent d'erreur,mauvais affichage,problème de droits,...)</p>
+<p><b><u>/!\</u> A faire Alexis/Joseph/Edouard:</b><br/><br/>Allez voir le fichier <b>missions.txt</b>, il y a une liste de missions plus ou moins longues à faire, classées par difficulté.</p>
+<p>&nbsp;Ecrivez dans ce fichier votre nom à côté de la mission que vous voulez effectuer et une fois finie, écrire "terminée votre_nom" devant.</p>
+<p>&nbsp;Faites un pull juste avant d'écrire dans le fichier et un push juste après pour que le fichier soit continuellement à jour.</p>
 <br/>
-<p><u>/!\</u>Trouver d'où vient le problème de la classe qui est pas trouvée dans Socket.php</p>
+<p><Trouver d'où vient le problème de la classe qui est pas trouvée dans Socket.php</p>
