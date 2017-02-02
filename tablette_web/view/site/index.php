a3a9e039e6d2b210c4b66897c92bb847b038c1fb
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2017-02-02 10:55:33 +0100

    Ajout des missions sur l' index

--- tablette_web/view/site/index.php
+++ tablette_web/view/site/index.php
@@ -3,17 +3,27 @@
 <p><i>(F) page à finaliser</i></p>
 <br/>
 <!--
-<p><b><u>/!\</u> A faire Alexis/Joseph/Edouard:</b><br/><br/>Allez voir le fichier <b>missions.txt</b>, il y a une liste de missions plus ou moins longues à faire, classées par difficulté.</p>
-<p>&nbsp;Ecrivez dans ce fichier votre nom à côté de la mission que vous voulez effectuer et une fois finie, écrire "terminée votre_nom" devant.</p>
-<p>&nbsp;Faites un pull juste avant d'écrire dans le fichier et un push juste après pour que le fichier soit continuellement à jour.</p>
-<br/>
+	<p><b><u>/!\</u> A faire Alexis/Joseph/Edouard:</b><br/><br/>Allez voir le fichier <b>missions.txt</b>, il y a une liste de missions plus ou moins longues à faire, classées par difficulté.</p>
+	<p>&nbsp;Ecrivez dans ce fichier votre nom à côté de la mission que vous voulez effectuer et une fois finie, écrire "terminée votre_nom" devant.</p>
+	<p>&nbsp;Faites un pull juste avant d'écrire dans le fichier et un push juste après pour que le fichier soit continuellement à jour.</p>
+	<br/>
 -->
 <!--
-<p>
-<b><u>/!\</u> A faire peut importe qui:</b><br/>
-<br/>
--Remplacer tous les input type="cancel" des formulaires par des <u>button</u><br/>
--Leur mettre le même CSS que les boutons d'avant (juste ajouter leur classe au bon endroit, et pas faire un nouveau bloc CSS)<br/>
--Ajouter un onclick header location machin truc pour rediriger au bon endroit
-</p>
+	Pas faire:
+	<p>
+		<b><u>/!\</u> A faire peu importe qui:</b><br/>
+		<br/>
+		-Remplacer tous les input type="cancel" des formulaires par des <u>button</u><br/>
+		-Leur mettre le même CSS que les boutons d'avant (juste ajouter leur classe au bon endroit, et pas faire un nouveau bloc CSS)<br/>
+		-Ajouter un onclick header location machin truc pour rediriger au bon endroit
+	</p>
 -->
+<p>
+	<b><u>/!\</u> A faire peu importe qui:</b><br/>
+	<br/>
+	-Finaliser la fonction Panier::getCoutTotal() c'est du SQL pur<br/>
+	<br/>
+	-Remplacer tous les number_format par une fonction affiche_cout qui fait le même traitement et qui rajoute '€' derrère<br/>
+	<br/>
+	-Faire une fonction qui remplace les " ' " d'une chaîne par un " \' " et l'appeller à chaque insertion/modification de BD<br/> 
+</p>
