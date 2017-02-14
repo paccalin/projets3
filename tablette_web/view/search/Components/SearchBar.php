8c48dc14551a98e2bd4b2a3e906281381dc803b0
paccalin <paccalin.axel@gmail.com>
2017-02-14 11:09:29 +0100

    création de la structure de gestrion 'propre' des requêtes ajax

--- tablette_web/view/search/Components/SearchBar.php
+++ tablette_web/view/search/Components/SearchBar.php
@@ -6,8 +6,12 @@
 		$returnValue .= "<form>";
 		$returnValue .= "<span class='icon'><i class='fa fa-search'></i></span>";
 
-      	$returnValue .= "<input type='search' id='searchtxt' placeholder='Search...' />";
 
+      	$returnValue .= "<input type='search' id='searchtxt' placeholder='Search...' />";
+            $returnValue .= "<br/><br/>";
+
+
+            $returnValue .= "<h3>Marque et modèles: </h3>";
       	$returnValue .= "<select id='constructeur' name='constructeur'>";
       	foreach ($pConstructeurList as $aConstructeur) {
       		$returnValue .= "<option value='".$aConstructeur->id." class='".$aConstructeur->libelle."'>".$aConstructeur->libelle."</option>";
@@ -19,6 +23,15 @@
       		$returnValue .= "<option value='".$aModele->id."' class='".$aModele->constructeur->libelle."'>".$aModele->libelle."</option>";
       	}
       	$returnValue .= "</select>";
+            $returnValue .= "<br/><br/>";
+
+
+            $returnValue .= "<h3>Types de produits: </h3>";
+            $returnValue .= "<input type='checkbox' id='cbox1' value='first_checkbox'> This is the first checkbox</label>";
+
+            $returnValue .= "<input type='checkbox' id='cbox2' value='second_checkbox'> <label for='cbox2'>This is the second checkbox</label>";
+
+
 
       	$returnValue .= "</form>";
 		$returnValue .= "</div>";
