42de3ccfab711a57c8b079a428ec0ff2384fe5a2
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2017-03-01 14:40:45 +0100

    On ne peut plus lier un client nul

--- app_centrale/tools.php
+++ app_centrale/tools.php
@@ -48,11 +48,11 @@ function removeQuote($st){
 
 function getIp(){
 	if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-			return $_SERVER['HTTP_CLIENT_IP'];
-		}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-			return _SERVER['HTTP_X_FORWARDED_FOR'];
-		}else{
-			return $_SERVER['REMOTE_ADDR'];
-		}
+		return $_SERVER['HTTP_CLIENT_IP'];
+	}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+		return $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}else{
+		return $_SERVER['REMOTE_ADDR'];
+	}
 }
 ?>
