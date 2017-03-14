e0b81d150bb90ba3a8f0c0abb22659561acf222e
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2017-03-14 18:42:30 +0100

    Ajout d'un fichiers SQL avec les données finales destinées au client

---
+++ tablette_web/dbScript/insertFinal.sql
@@ -0,0 +1,26 @@
+/*=== Client ===*/
+
+
+/*=== Constructeur ===*/
+
+
+/*=== Typemodele ===*/
+
+
+/*=== Modele ===*/
+
+
+/*=== Typeoption ===*/
+
+
+/*=== Options ===*/
+
+
+/*=== Vehicule ===*/
+
+
+/*=== Photo ===*/
+
+
+/*=== Utilisateur ===*/
+insert into utilisateur values ('i40AX3l6f0rD4WNv15xT', 'superadministrateur', 'idam123', 3, CURRENT_TIMESTAMP);
